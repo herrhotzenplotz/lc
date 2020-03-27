e6ce9ac23b8b38273928f7ca04f532f798cb818e
Nico Sonack <34663024+herrhotzenplotz@users.noreply.github.com>
2020-03-27 19:11:39 +0100

    Hindent

--- src/Main.hs
+++ src/Main.hs
@@ -11,73 +11,81 @@ import Control.Monad.Trans.State.Strict
 import Data.Char
 import qualified Data.Map.Strict as M
 import Data.Version (showVersion)
-import System.IO (hFlush, stdout)
 import Paths_lc (version)
+import System.IO (hFlush, stdout)
 
 data Term
-    = Variable String
-    | Application Term Term
-    | Abstraction String Term
-    | Closure String Term Scope
-      deriving (Show, Eq)
+  = Variable String
+  | Application Term Term
+  | Abstraction String Term
+  | Closure String Term Scope
+  deriving (Show, Eq)
 
 data InterpreterError
-    = SyntaxError String
-    | SemanticError String
-      deriving Show
+  = SyntaxError String
+  | SemanticError String
+  deriving (Show)
 
-newtype Parser a
-    = Parser {
-        runParser :: String -> Either InterpreterError (String, a)
-      } deriving Functor
+newtype Parser a =
+  Parser
+    { runParser :: String -> Either InterpreterError (String, a)
+    }
+  deriving (Functor)
 
 type Scope = M.Map String Term
 
 type InterpreterResult = Either InterpreterError Term
 
 data InterpreterCommand
-    = LetBinding String Term
-    | Release String
-    | EvalCommand Term
+  = LetBinding String Term
+  | Release String
+  | EvalCommand Term
 
 instance Applicative Parser where
-    pure x = Parser $ \input -> Right (input, x)
-    Parser f <*> Parser p = Parser $ \input -> do
-                              (input', f') <- f input
-                              (input'', res) <- p input'
-                              return (input'', f' res)
+  pure x = Parser $ \input -> Right (input, x)
+  Parser f <*> Parser p =
+    Parser $ \input -> do
+      (input', f') <- f input
+      (input'', res) <- p input'
+      return (input'', f' res)
 
 instance Alternative (Either InterpreterError) where
-    empty = Left $ SemanticError "Empty Parser"
-    Left _ <|> err = err
-    ei1 <|> _ = ei1
+  empty = Left $ SemanticError "Empty Parser"
+  Left _ <|> err = err
+  ei1 <|> _ = ei1
 
 instance Alternative Parser where
-    empty = Parser $ const empty
-    (Parser p1) <|> (Parser p2) = Parser $ \x -> p1 x <|> p2 x
+  empty = Parser $ const empty
+  (Parser p1) <|> (Parser p2) = Parser $ \x -> p1 x <|> p2 x
 
 instance Monad Parser where
-    (Parser p1) >>= f = Parser $ \input -> do
-                          (input', a) <- p1 input
-                          runParser (f a) input'
+  (Parser p1) >>= f =
+    Parser $ \input -> do
+      (input', a) <- p1 input
+      runParser (f a) input'
 
 instance MonadFail Parser where
-    fail m = Parser $ const $ Left $ SyntaxError m
+  fail m = Parser $ const $ Left $ SyntaxError m
 
 expectChar :: Char -> Parser ()
-expectChar c = Parser $ \case
-               (c'):rest | c' == c -> Right (rest, ())
-               _ -> Left $ SyntaxError ("Expected '" <> [c] <> "'")
+expectChar c =
+  Parser $ \case
+    (c'):rest
+      | c' == c -> Right (rest, ())
+    _ -> Left $ SyntaxError ("Expected '" <> [c] <> "'")
 
 expectOP :: Parser ()
 expectOP = expectChar '('
 
 ws :: Parser ()
-ws = void $
-     many $
-     Parser (\case
-              x:xs | isSpace x -> Right (xs, ())
-              _ -> Left $ SyntaxError "Expected whitespace")
+ws =
+  void $
+  many $
+  Parser
+    (\case
+       x:xs
+         | isSpace x -> Right (xs, ())
+       _ -> Left $ SyntaxError "Expected whitespace")
 
 expectCP :: Parser ()
 expectCP = expectChar ')'
@@ -89,17 +97,19 @@ expectDot :: Parser ()
 expectDot = expectChar '.'
 
 parseIdentifier :: Parser String
-parseIdentifier = Parser $ \inp ->
-                  case span isAlphaNum inp of
-                    ([], rest) -> Left $ SyntaxError ("Expected an identifier but got '" <> rest)
-                    (identifier, rest) -> Right (rest, identifier)
+parseIdentifier =
+  Parser $ \inp ->
+    case span isAlphaNum inp of
+      ([], rest) ->
+        Left $ SyntaxError ("Expected an identifier but got '" <> rest)
+      (identifier, rest) -> Right (rest, identifier)
 
 expectString :: String -> Parser ()
 expectString expect = do
   got <- parseIdentifier
   if got == expect
-  then return ()
-  else fail $ "Expected '" <> expect <> "' bot got '" <> got <> "'"
+    then return ()
+    else fail $ "Expected '" <> expect <> "' bot got '" <> got <> "'"
 
 parseApplication :: Parser Term
 parseApplication = do
@@ -121,25 +131,24 @@ parseVariable :: Parser Term
 parseVariable = Variable <$> parseIdentifier
 
 parseTerm :: Parser Term
-parseTerm = ws *> (parseApplication <|> parseAbstraction <|> parseVariable) <* ws
+parseTerm =
+  ws *> (parseApplication <|> parseAbstraction <|> parseVariable) <* ws
 
 interpretScoped :: Scope -> Term -> InterpreterResult
-interpretScoped scope (Variable v) = case M.lookup v scope of
+interpretScoped scope (Variable v) =
+  case M.lookup v scope of
     Just term -> return term
     Nothing -> Left $ SemanticError ("Unbound value '" <> v <> "'")
-
 interpretScoped scope (Abstraction arg body) = Right $ Closure arg body scope
-
 interpretScoped scope (Application f val) = do
-    fEval <- interpretScoped scope f
-    case fEval of
-      Closure f' body closedScope -> do
-          valEval <- interpretScoped scope val
-          let newScope = M.insert f' valEval closedScope
-          let forEvaluation = M.union scope newScope
-          interpretScoped forEvaluation body
-      _ -> Left $ SemanticError "Cannot apply non-function values"
-
+  fEval <- interpretScoped scope f
+  case fEval of
+    Closure f' body closedScope -> do
+      valEval <- interpretScoped scope val
+      let newScope = M.insert f' valEval closedScope
+      let forEvaluation = M.union scope newScope
+      interpretScoped forEvaluation body
+    _ -> Left $ SemanticError "Cannot apply non-function values"
 interpretScoped _ closure = return closure
 
 printT :: Term -> String
@@ -175,8 +184,7 @@ release binding = do
     True -> do
       put $ M.delete binding globalScope
       lift $ putStrLn $ ":: '" <> binding <> "' has been released."
-    False ->
-      lift $ putStrLn $ "?: '" <> binding <> "': No such binding ."
+    False -> lift $ putStrLn $ "?: '" <> binding <> "': No such binding ."
 
 printError :: InterpreterError -> StateT Scope IO ()
 printError err = lift $ putStrLn $ "?: " <> printE err
@@ -197,7 +205,8 @@ parseEvalCommand :: Parser InterpreterCommand
 parseEvalCommand = EvalCommand <$> parseTerm
 
 parseInterpreterCommand :: Parser InterpreterCommand
-parseInterpreterCommand = parseLetBinding <|> parseReleaseCommand <|> parseEvalCommand
+parseInterpreterCommand =
+  parseLetBinding <|> parseReleaseCommand <|> parseEvalCommand
 
 repl :: StateT Scope IO ()
 repl = do
@@ -208,11 +217,11 @@ repl = do
   case p of
     Left err -> printError err
     Right (resStr, command) -> do
-        case command of
-          EvalCommand term -> eval term
-          LetBinding binding term -> bind binding term
-          Release binding -> release binding
-        if resStr == []
+      case command of
+        EvalCommand term -> eval term
+        LetBinding binding term -> bind binding term
+        Release binding -> release binding
+      if resStr == []
         then return ()
         else lift $ putStrLn $ "Warning: Incomplete parse: " <> resStr
   repl
