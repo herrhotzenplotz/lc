a23024197d1bd41dca3651371b6352889f7c19e6
Nico Sonack <34663024+herrhotzenplotz@users.noreply.github.com>
2020-03-28 16:18:26 +0100

    Add file loading mechanism

--- src/Main.hs
+++ src/Main.hs
@@ -17,32 +17,46 @@ import System.Console.ANSI
   , SGR(..)
   , setSGR
   )
+import System.Environment (getArgs)
 import System.IO (hFlush, stdout)
 
-repl :: StateT Scope IO ()
-repl = do
-  lift $ putStr "> "
-  lift $ hFlush stdout
-  inp <- lift getLine
-  let p = runParser parseInterpreterCommand inp
-  case p of
-    Left err -> printError err
-    Right (resStr, command) -> do
-      case command of
-        EvalCommand term -> eval term
-        LetBinding binding term -> bind binding term
-        Release binding -> release binding
-      if null resStr
+runLine :: String -> StateT Scope IO ()
+runLine inp =
+    case runParser parseInterpreterCommand inp of
+      Left err -> printError err
+      Right (resStr, command) -> do
+        case command of
+          EvalCommand term -> eval term
+          LetBinding binding term -> bind binding term
+          Release binding -> release binding
+        if null resStr
         then return ()
         else lift $ do
                setSGR [SetColor Foreground Vivid Magenta]
                putStrLn $ "Warning: Incomplete parse: " <> resStr
                setSGR []
+
+repl :: StateT Scope IO ()
+repl = do
+  lift $ putStr "> "
+  lift $ hFlush stdout
+  lift getLine >>= runLine
   repl
+
+interpretFile :: String -> StateT Scope IO ()
+interpretFile filePath = do
+  lift $ putStrLn $ "Loading file '" <> filePath <> "'"
+  code <- lift $ lines <$> readFile filePath
+  mapM_ runLine code
+
+mainWithArgs :: [String] -> IO ()
+mainWithArgs args = do
+  (_, replState) <- runStateT (mapM_ interpretFile args) M.empty
+  fst <$> runStateT repl replState
 
 main :: IO ()
 main = do
   putStrLn "lc - untyped lambda calculus interpreter"
   putStrLn $ "Version " <> showVersion version
   putStrLn "Copyright 2020 by Nico Sonack\n"
-  fst <$> runStateT repl M.empty
+  getArgs >>= mainWithArgs
